19bd704579d5b47258121deb250f3c9ee5300250
Kim Seung Hwan <dev.appmin@gmail.com>
2022-05-15 22:58:16 +0900

    feat: Setting up the react route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Detail from "./routers/Detail";
+import Home from "./routers/Home";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
