2f3166c0450f55b284d08e2aa8cce8b61f4b987a
Kim Seung Hwan <dev.appmin@gmail.com>
2022-05-17 23:41:26 +0900

    feat: Implement the footer

--- src/routers/Home.tsx
+++ src/routers/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <div style={{ height: 1500 }} />
+      <Footer />
     </div>
   );
 };
