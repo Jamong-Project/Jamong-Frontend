cbaa10cd2391523c2cdeb4d31354c88d7f8f12e7
Kim Seung Hwan <dev.appmin@gmail.com>
2022-05-24 18:31:10 +0900

    feat: Implement the volunteer list

--- src/routers/Volunteer.tsx
+++ src/routers/Volunteer.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import VolunteerList from "../components/VolunteerList";
 
 const Volunteer = () => {
   return (
-    <div>
-      <h1>Volunteer</h1>
-    </div>
+    <>
+      <Header />
+      <VolunteerList />
+      <Footer />
+    </>
   );
 };
 
