a1b41cc25ab220668363cd3b00c850746173cb2b
Kim Seung Hwan <dev.appmin@gmail.com>
2022-06-11 00:49:04 +0900

    feat: Get data from components and post to server

--- src/routers/volunteer-post.tsx
+++ src/routers/volunteer-post.tsx
@@ -1,27 +1,32 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Footer from "../components/common/footer";
 import Header from "../components/common/header";
 import HeaderTopSpace from "../components/common/header-top-space";
 import ImageUploader from "../components/image-uploader";
 import MilkdownEditor from "../components/milkdown/milkdown-editor";
+import PostButton from "../components/post-button";
 import TitleUploader from "../components/title-uploader";
 import VolunteerToolBar from "../components/volunteer-tool-bar";
 import { VolunteerPostType } from "../models/volunteer-post-type";
+import { timestampToUnixTimestamp } from "../utils";
+
+const formData = new FormData();
 
 const VolunteerPost = () => {
-  const formData = new FormData();
   const [requestBody, setRequestBody] = useState<VolunteerPostType>({
     title: "",
     content: "",
-    volunteerDate: new Date().getTime(),
-    applicationDate: new Date().getTime(),
+    volunteerDate: timestampToUnixTimestamp(new Date().getTime()),
+    applicationDate: timestampToUnixTimestamp(new Date().getTime()),
     maximumPeople: 0,
   });
 
-  const requestCallback = (key: string, value: any) => {
-    setRequestBody((prev) => ({ ...prev, [key]: value }));
-  };
+  const requestCallback = useCallback(
+    (key: string, value: any) => {
+      setRequestBody((prev) => ({ ...prev, [key]: value }));
+    },
+    [requestBody],
+  );
 
   return (
     <>
@@ -29,8 +34,9 @@ const VolunteerPost = () => {
       <HeaderTopSpace />
       <TitleUploader callback={requestCallback} />
       <VolunteerToolBar callback={requestCallback} />
-      <MilkdownEditor />
+      <MilkdownEditor callback={requestCallback} />
       <ImageUploader formData={formData} />
+      <PostButton formData={formData} requestBody={requestBody} />
       <Footer />
     </>
   );
