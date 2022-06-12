62983a95c94bf37901957e065bc08a3e01c5e94a
Kim Seung Hwan <dev.appmin@gmail.com>
2022-06-12 17:22:36 +0900

    refactor: Replace fetch to axios

--- src/hooks/use-fetch-data.ts
+++ src/hooks/use-fetch-data.ts
@@ -1,27 +1,28 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-const useFetchData = (url: string) => {
+const useFetchData = (url: string, config?: object) => {
   const [data, setData] = useState<any>();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<any>();
+  const [header, setHeader] = useState<any>();
 
   useEffect(() => {
     setLoading(true);
-    fetch(url)
+    axios
+      .get(url, config)
       .then((response) => {
-        return response.json();
-      })
-      .then((d) => {
-        setData(d);
+        setData(response.data);
+        setHeader(response.headers);
         setLoading(false);
       })
       .catch((e) => {
         setError(e);
         setLoading(false);
       });
-  }, [url]);
+  }, [url, config]);
 
-  return [data, loading, error];
+  return [data, loading, error, header];
 };
 
 export default useFetchData;
