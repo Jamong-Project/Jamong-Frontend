bbbdcc5cc6e61f03a36c407168c541627a9b385c
Kim Seung Hwan <dev.appmin@gmail.com>
2022-06-23 01:39:40 +0900

    fix: Modify naver login callback url to access login outside of the local server

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
   };
 
   const loggedOutCallback = () => {
-    const accessToken = localStorage.getItem("localStorage");
+    const accessToken = localStorage.getItem("com.naver.nid.access_token");
 
     if (!accessToken) return;
 
