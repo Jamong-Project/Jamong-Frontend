3c7996a041214077ae698c11404989a97cbb9509
Kim Seung Hwan <dev.appmin@gmail.com>
2022-06-24 01:33:08 +0900

    feat: Auto re-direct to home route after login is finished

--- src/routers/login/naver-auth/index.tsx
+++ src/routers/login/naver-auth/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 
 const ACCESS_TOKEN_KEY: string = "access_token";
 
 const NaverAuth = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (location.hash.includes(ACCESS_TOKEN_KEY)) {
@@ -13,26 +14,11 @@ const NaverAuth = () => {
         location.hash.split("=")[1].split("&")[0] ?? "none",
       );
     }
+
+    navigate("/");
   }, []);
 
-  return (
-    <div>
-      <h1>Auth Complete!</h1>
-      <h2>
-        This page is temporary success site. The token will be sended to the
-        server.
-      </h2>
-      <p>{location.hash}</p>
-      <button
-        type="button"
-        onClick={() => {
-          window.location.href = "/";
-        }}
-      >
-        Go to Home
-      </button>
-    </div>
-  );
+  return <div />;
 };
 
 export default NaverAuth;
